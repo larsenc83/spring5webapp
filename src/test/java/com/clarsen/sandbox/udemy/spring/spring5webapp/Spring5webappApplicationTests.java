28f879bcacea7019e14855468514c62c54b3a22f
larsenc83 <larsenc83@gmail.com>
2022-03-12 11:08:57 -0500

    starting source

---
+++ src/test/java/com/clarsen/sandbox/udemy/spring/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarsen.sandbox.udemy.spring.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
