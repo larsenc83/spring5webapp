27648a73c231401e3bb7d74b0c6727f9c2134163
larsenc83 <larsenc83@gmail.com>
2022-03-12 14:16:06 -0500

    add un-resolved type

--- src/main/java/com/clarsen/sandbox/udemy/spring/spring5webapp/domain/Book.java
+++ src/main/java/com/clarsen/sandbox/udemy/spring/spring5webapp/domain/Book.java
@@ -14,6 +14,7 @@ import javax.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
